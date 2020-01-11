90f68d7cad1ad0931c4c652ce943e88aefdc75cf
TerpugovaMasha <56717263+TerpugovaMasha@users.noreply.github.com>
2020-01-11 13:53:30 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
 
-        System.out.print("ВВедите диапозон значений элементов например (2,5):  ");
+        System.out.print("ВВедите диапозон значений элементов например (1,5):  ");
         String range = sc.next();
 
-        String[] ranges = range.split( ",");
+        String[] ranges = range.split(",");
         System.out.println(Arrays.toString(ranges));
         int low = Integer.parseInt(ranges[0]);
         int high = Integer.parseInt(ranges[1]);
 
-        Algorythm algorythm = new Algorythm();
-        algorythm.start(count, low, high);
-        int[] array = algorythm.getArray();
-        System.out.println(Arrays.toString(array));
+        Algorythm algorythm = new Algorythm(count, low, high);
+        algorythm.start();
+		
+        System.out.println(Arrays.toString(algorythm.getArray()));
     }
 }
